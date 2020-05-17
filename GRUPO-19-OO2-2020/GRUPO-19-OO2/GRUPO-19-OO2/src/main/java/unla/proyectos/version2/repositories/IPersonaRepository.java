210a20b333681eb8be6c7155b026d3216927548b
jemi14 <griselda_gsis@hotmail.com>
2020-05-16 22:11:09 -0300

    Subo de nuevo de mi parte
    Ayuda no me sale :(

---
+++ GRUPO-19-OO2-2020/GRUPO-19-OO2/GRUPO-19-OO2/src/main/java/unla/proyectos/version2/repositories/IPersonaRepository.java
@@ -0,0 +1,23 @@
+package unla.proyectos.version2.repositories;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import unla.proyectos.version2.entities.Persona;
+
+
+
+@Repository("personaRepository")
+public interface IPersonaRepository extends JpaRepository<Persona, Serializable>{
+	 public abstract Persona findByIdPersona(long id);
+	public abstract Persona findByDni(long dni);
+
+
+	 
+	
+	 
+	
+}
